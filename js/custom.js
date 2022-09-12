84a034062c989d0ad3da8f0aa713dba87f3e0fb2
Lain Drion <igencap@gmail.com>
2022-09-12 15:50:45 +0500

    Header almost ready. Overall Second version of website

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,57 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-    // CUSTOM LINK 
-    $('.custom-link').click(function(){
-    var el = $(this).attr('href');
+  // CUSTOM LINK
+  $(".custom-link").click(function () {
+    var el = $(this).attr("href");
     var elWrapped = $(el);
-    var header_height = $('.navbar').height() + 10;
+    var header_height = $(".navbar").height() + 10;
 
-    scrollToDiv(elWrapped,header_height);
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
-  }
-});
-    
-  })(window.jQuery);
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+  // Dropdown menu
+  $(function () { // Dropdown toggle
+    $('.lang__toggle').click(function () {
+      $(this).next('.menu__lang').slideToggle();
+    });
+
+    $(document).click(function (e) {
+      var target = e.target;
+      if (!$(target).is('.lang__toggle') && !$(target).parents().is('.lang__toggle'))
+      //{ $('.dropdown').hide(); }
+      { $('.menu__lang').slideUp(); }
+    });
+  });
 
 
+  $(".news__list-box").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    prevArrow: '.arrow__prev',
+    nextArrow: '.arrow__next',
+  });
+
+
+})(window.jQuery);
+
+// $(function () {});
