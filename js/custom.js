68dddcd488ff276a014d3db76ffc8cc6d5311aa6
Lain Drion <igencap@gmail.com>
2022-11-03 18:02:34 +0500

    Don't like the links, map contact seciotns

--- js/custom.js
+++ js/custom.js
@@ -88,11 +88,13 @@
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     arrows: false,
     autoplay: true,
     autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: '60px'
   })
 
   // SIDEBAR SLIDER
