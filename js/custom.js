d84b9e38a785ea60779a2027738ff06cb15b1c9f
Lain Drion <igencap@gmail.com>
2022-11-11 18:11:19 +0500

    need to fulfill and style vacancy.html

--- js/custom.js
+++ js/custom.js
@@ -106,3 +106,20 @@ $(function () {
     $.magnificPopup.close();
   });
 });
+
+// Vacancy
+
+var acc = document.getElementsByClassName("vacancy__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
