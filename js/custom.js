1027e829247c15c93e455baf5e9613754c1ea551
Lain Drion <igencap@gmail.com>
2022-12-14 12:28:52 +0500

    done with request-4

--- js/custom.js
+++ js/custom.js
@@ -113,26 +113,67 @@
       }
     });
   }
+  
+  // EVENTS
+  $('.events__date-slider-box').slick({
+    slidesToShow: 7,
+    slidesToScroll: 6,
+    infinite: false,
+    prevArrow: $('.events__arrow-left'),
+    nextArrow: $('.events__arrow-right'),
+    speed: 800
+
+  })
 
 
   // COSTS
 
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
+  const pie1 = document.getElementById('myChart');
+  var data1 = {
+    labels: ['Конкурс: 19.1% (197,000,000)', 'Каталог: 18.1% (186, 293, 382)', 'Аукцион: 62.8% (646, 899, 680)'],
+    datasets: [{
+      data: [19.1, 18.1, 62.8],
+      backgroundColor: ["#6771DC", "#67B7DC", "#6794DC"],
+      hoverBackgroundColor: ["#717CF3", "#6DC2EA", "#71A3F3"],
+      borderWidth: 1
+    }]
+  };
+  new Chart(pie1, {
     type: 'pie',
-    data: {
-      labels: ['Конкурс: 19.1%', 'Каталог: 18.1%', 'Аукцион: 62.8%'],
-      datasets: [{
-        data: [19.1, 18.1, 62.8],
-        borderWidth: 1
-      }]
-    },
     plugins: {
       legend: {
-        display: false
+        display: false,
+      },
+      tooltip: {
+        enabled: false
       }
-    }
+    },
+    data: data1
+  });
+
+  const pie2 = document.getElementById('myChart2');
+  var data2 = {
+    labels: ['Аукцион: 79.6 % (89,799,200)', 'Каталог: 12.4% (13, 990, 694)', 'Конкурс: 9% (9, 000, 000)'],
+    datasets: [{
+      data: [79.6, 12.4, 9],
+      backgroundColor: ["#6771DC", "#67B7DC", "#6794DC"],
+      hoverBackgroundColor: ["#717CF3", "#6DC2EA", "#71A3F3"],
+      borderWidth: 1
+    }]
+  };
+
+  new Chart(pie2, {
+    type: 'pie',
+    plugins: {
+      legend: {
+        display: false,
+        // position: left
+      },
+      tooltip: {
+        enabled: false
+      }
+    },
+    data: data2
   });
 
 })(window.jQuery);
